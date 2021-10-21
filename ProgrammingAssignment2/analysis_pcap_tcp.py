f67d87e617fe7d514839d0dfa4e85ac93f2e57ca
Tahmidul Alam <31861624+AlamTahmidul@users.noreply.github.com>
2021-10-21 12:01:19 -0400

    Part A complete (Need to output)
    TODO: Write to file the output for Part A

--- ProgrammingAssignment2/analysis_pcap_tcp.py
+++ ProgrammingAssignment2/analysis_pcap_tcp.py
@@ -1,24 +1,25 @@
-from os import error
-import dpkt
 from socket import inet_ntoa
+import dpkt
+from dpkt.tcp import TH_SYN, TH_ACK, TH_FIN
 
 class Packet:
+    tcp_flows = 0 # Counter for Complete TCP flows (SYN - FIN)
     def __init__(self, src=0, dst=0, sport=80, dport=80):
         self.src = src
         self.dst = dst
         self.sport = sport
         self.dport = dport
-        self.syn_1 = False # First Connection (1 of 3)
+        self.st = 0 # TOTAL Sender Throughput
+        self.syn = False # First Connection (1 of 3)
+        self.syn_time = 0
         self.syn_ac_1 = False # First Connection ACK (2 of 3)
-        self.ac_1 = False # Sending ACK back (3 of 3)
+        self.ac_1 = False
         self.fin_found = False # Checking if fin exists
-        self.stor_data = [] # List of tuples from sender to receiver; (SEQ, ACK, WindowSize)
-        self.rtos_data = [] # List of tuples from receiver to sender
-
-    def check(self, src, dst, sport, dport):
-        if self.src == src and self.dst == dst and self.sport == sport and self.dport == dport:
-            return True
-        return False
+        self.retrans = [0, 0, 0] # [Trip-Dup, Timeout, Counter]
+        self.win_scale = 1 # Get the win-scale
+        self.stor_data = [] # List of tuples from sender to receiver; {"TIME", "SEQ", "ACK", "WINDOW_SIZE", "LENGTH", "TYPE"}
+        self.rtos_data = [] # List of tuples from receiver to sender; {"TIME", "SEQ", "ACK", "WINDOW_SIZE", "LENGTH", "TYPE"}
+        self.count = 0 # Debugging Purposes
 
 
 packets = []
@@ -27,69 +28,81 @@ def find(src, dst, sport, dport):
     loc = 0
     for p in packets:
         if (p.src == src and p.dst == dst and p.sport == sport and p.dport == dport): # Sender to Receiver
-            return (loc, False)
-        elif (p.src == dst and p.dst == src and p.sport == dport and p.dport == sport): # Receiver to Sender
             return (loc, True)
+        elif (p.src == dst and p.dst == src and p.sport == dport and p.dport == sport): # Receiver to Sender
+            return (loc, False)
         loc += 1
     return (-1, False)
 
-# TODO: get the connection setup ->  For the first two transaction after the TCP connection is set up (from sender to receiver), the values of the Sequence number, Ack number, and Receive Window size -> Sender throughput
 def parse_pcap(pcap_file_reader):
     for (timestamp, buffer) in pcap_file_reader:
         try:
             ethernet = dpkt.ethernet.Ethernet(buffer)
-            ip = ethernet.data
-            tcp = ip.data
+            ip: dpkt.ip.IP = ethernet.data
+            tcp: dpkt.tcp.TCP = ip.data
             if (type(tcp) == dpkt.tcp.TCP):
-                loc_stor, stor = find(ip.src, ip.dst, tcp.sport, tcp.dport)
-                loc_rtos, rtos = find(ip.dst, ip.src, tcp.dport, tcp.sport)
-                if (stor and loc_stor >= 0):
-                    # FLAGS: 0x002 -> SYN, 0x012 -> SYN/ACK, 0x010 -> ACK, 0x018 -> PSH/ACK, 0x011 -> FIN/ACK, 0x019 -> FIN/PSH/ACK
-                    src_ip, src_port = (inet_ntoa(ip.src), tcp.sport)
-                    dst_ip, dst_port = (inet_ntoa(ip.dst), tcp.dport)
-                    pack: Packet = packets[stor]
+                loc_stor, stor = find(ip.src, ip.dst, tcp.sport, tcp.dport) # Sender to receiver -> Negate stor to get Receiver to Sender
 
-                    if tcp.flags == 0x012: # SYN/ACK (2 of 3)
-                        print(f"(SYN/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port} and modified Packet var")
-                        pack.syn_ac_1 = True
-                    elif tcp.flags == 0x010: # ACK (3 of 3 /Or-> Sending Back/Receiving ACK)
-                        # print(f"(ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                        if (not pack.ac_1):
+                if (stor and loc_stor >= 0): # Sender to receiver (stor)
+                    pack: Packet = packets[loc_stor] # Get the object for sender-receiver pair
+                    
+                    src_ip, src_port = (inet_ntoa(pack.src), pack.sport)
+                    dst_ip, dst_port = (inet_ntoa(pack.dst), pack.dport)
+
+                    if ((tcp.flags & TH_ACK) and not (tcp.flags & TH_SYN) and not pack.fin_found): # ACK
+                        pack.stor_data.append( {"TIME": timestamp, "SEQ": tcp.seq, "ACK": tcp.ack, "WINDOW_SIZE": tcp.win, "LENGTH": len(tcp), "TYPE": "ACK"} )
+                        if not pack.ac_1: # Did not receive acknowledgement yet
                             pack.ac_1 = True
                         else:
-                            # TODO: APPEND
-                            # pack.stor_data.append()
-                            pass
-                    elif tcp.flags == 0x011:
-                        print(f"(FIN/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                    elif tcp.flags == 0x019:
-                        print(f"(FIN/PSH/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                elif (rtos):
-                    src_ip, src_port = (inet_ntoa(ip.dst), tcp.dport)
-                    dst_ip, dst_port = (inet_ntoa(ip.src), tcp.sport)
-                    
-                    if tcp.flags == 0x012: # SYN/ACK (2 of 3)
-                        print(f"(SYN/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                    elif tcp.flags == 0x010: # ACK (3 of 3 /Or-> Sending Back/Receiving ACK)
+                            pack.st += len(tcp)
                         # print(f"(ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                        pass
-                    elif tcp.flags == 0x011:
-                        print(f"(FIN/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                    elif tcp.flags == 0x019:
-                        print(f"(FIN/PSH/ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
-                else:
-                    # Convert 32-bit IPV4 to standard dotted string decimal (127.0.0.1)
-                    pack = Packet(ip.src, ip.dst, tcp.sport, tcp.dport)
-                    if tcp.flags == 0x002: # IF IT's A SYN -> 1 of 3 of 3-way handsake
-                        pack.syn1 = True
-                        print
-                    packets.append(pack)
-                    # print(f"Source: {inet_ntoa(ip.src)} on Port {tcp.sport}, Destination: {inet_ntoa(ip.dst)} on Port {tcp.dport}")
+                elif ((not stor) and loc_stor >= 0): # Receiver to sender (rtos)
+                    pack: Packet = packets[loc_stor] # Get the object for sender-receiver pair
+                    
+                    src_ip, src_port = (inet_ntoa(pack.dst), pack.dport)
+                    dst_ip, dst_port = (inet_ntoa(pack.src), pack.sport)
+                    
+                    if ((tcp.flags & TH_SYN) and (tcp.flags & TH_ACK) and not (tcp.flags & TH_FIN)): # SYN/ACK (2 of 3)
+                        pack.syn_ac_1 = True
+                        # print(f"(SYN/ACK) Source: {src_ip}:{src_port}, Destination: {dst_ip}:{dst_port}")
+                    elif ((tcp.flags & TH_ACK) and not (tcp.flags & TH_FIN) and not (tcp.flags & TH_SYN)): # ACK
+                        pack.rtos_data.append({"TIME": timestamp, "SEQ": tcp.seq, "ACK": tcp.ack, "WINDOW_SIZE": tcp.win, "LENGTH": len(tcp), "TYPE": "ACK"})
+                        # print(f"(ACK) Source: {src_ip} on Port {src_port}, Destination: {dst_ip} on Port {dst_port}")
+                    elif ( tcp.flags & TH_FIN ): # FIN/ACK
+                        pack.fin_found = True
+                        pack.rtos_data.append( {"TIME": timestamp, "SEQ": tcp.seq, "ACK": tcp.ack, "WINDOW_SIZE": tcp.win, "LENGTH": len(tcp), "TYPE": "FIN/ACK"} )
+                        if (pack.syn):
+                            Packet.tcp_flows += 1
+                        print(f"(FIN/ACK) Source: {src_ip}:{src_port}, Destination: {dst_ip}:{dst_port}")
+                else: # COULD BE A SYN (i.e. START OF A NEW TCP FLOW)
+                    if ( (tcp.flags & TH_SYN) and not (tcp.flags & TH_ACK) and not (tcp.flags & TH_FIN)): # IF IT's A SYN -> 1 of 3 of 3-way handsake
+                        # Convert 32-bit IPV4 to standard dotted string decimal (127.0.0.1)
+                        pack = Packet(src=ip.src, dst=ip.dst, sport=tcp.sport, dport=tcp.dport)
+                        pack.syn = True
+                        pack.syn_time = timestamp
+                        pack.win_scale = int.from_bytes(tcp.opts.strip()[-1:], "big") # LAST BYTE HAS WINDOW SCALE
+                        packets.append(pack)
+                        print(f"(SYN) Source: {inet_ntoa(ip.src)}:{tcp.sport}, Destination: {inet_ntoa(ip.dst)}:{tcp.dport}")
         except Exception as e:
-            # print("Error found!")
-            print(e)
+            print("Error found! Exiting...")
+            # print(e)
             exit(-1)
 
+# Write to File the Analysis
+def analyze():
+    with open("analysis.txt", "w") as f:
+        f.write("Number of complete TCP Flows (SYN with FIN): " + Packet.tcp_flows)
+        for p in packets:
+            pack: Packet = p
+            i_offset = 0
+            
+            if pack.ac_1:
+                i_offset = 1
+            
+
+
+            pass
+        pass
 
 def run(pcap_file):
     try:
@@ -105,3 +118,11 @@ if __name__ == "__main__":
     # file_loc = input("Enter The directory of the file: ")
     file_loc = "assignment2.pcap"
     run(pcap_file=file_loc)
+    print(Packet.tcp_flows)
+
+    for p in packets:
+        # manual_sum = p.stor_data[-1]["SEQ"] - p.stor_data[0]["SEQ"]
+        time = p.rtos_data[-1]["TIME"] - p.stor_data[1]["TIME"]
+        time1 = p.stor_data[1]["TIME"] - p.syn_time
+        time2 = p.rtos_data[-1]["TIME"] - p.syn_time
+        print(f"Sum: {p.st},  Number of Sender Flows: {len(p.stor_data)}, Time: {time}, First Time: {time1}, Second Time: {time2}, Count: {p.count}")
